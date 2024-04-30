1d3eaf353a452b5c98c52077086134e9971b71b0
Hudralisk <168286823+A-Lexuz@users.noreply.github.com>
2024-04-30 17:15:31 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,7 @@
+my_string = input ('Введите имя строки: ')
+print(len(my_string)) # Длина строки
+print(my_string.upper()) # Строка в верхнем регистре
+print(my_string.lower()) # Строка в нижнем регистре
+print(my_string.replace(' ', '')) # Строка без пробелов
+print(my_string[0]) # первый символ строки
+print(my_string[-1]) # последний символ строки
