8642f1da91409f838f43e9977dbedf4fbf947871
Hudralisk <168286823+A-Lexuz@users.noreply.github.com>
2024-05-09 20:39:01 +0300

    Add files via upload

---
+++ Homework2_2.py
@@ -0,0 +1,14 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in cars:
+    print ('Я езжу на автомобиле марки', i)
+
+cars_count = 0
+for i in cars:
+    cars_count += 10
+    print (cars_count)
+
+ages = [13, 15, 19, 15, 14]
+for i in range(len(ages)):
+    ages[i] += 1
+
+print (ages)
