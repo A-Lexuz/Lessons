3fa16a669d73e12e7bec7ded87508ecf02b338c8
Hudralisk <168286823+A-Lexuz@users.noreply.github.com>
2024-10-24 22:23:08 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -9,17 +10,20 @@ async def all_users() -> dict:
     return users
 
 @app.post('/user/{username}/{age}')
-async def add_user(username:str, age:int) -> str:
+async def add_user(username: Annotated[str, Path(min_length=5,max_length=20,description="Enter username",example='username')],
+                   age: Annotated[int,  Path(ge=18,le=120,description="Enter age",example='24')]) -> str:
     current_id = str(int(max(users, key=int)) + 1)
     users[current_id] = f'Имя:{username}, возраст: {age}'
     return f'User {current_id} is registered'
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id:str, username:str, age:int) -> str:
-    users[user_id] = f'Имя:{username}, возраст: {age}'
+async def update_user(user_id: Annotated[int, Path(ge=0, le=100, description='Enter User ID', example=1)],
+                      username: Annotated[str, Path(min_length=5,max_length=20,description="Enter username",example='username')],
+                      age: Annotated[int,Path(ge=18,le=120,description="Enter age",example='24')]) -> str:
+    users[user_id] = f'Имя:{username}, возраст: {str(age)}'
     return f'User {user_id} is registered'
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: str) -> str:
-    users.pop(user_id)
+async def delete_user(user_id: int = Path(ge=0, le=100, description='Enter User ID', example=1)) -> str:
+    users.pop(str(user_id))
     return f'User {user_id} was deleted'
